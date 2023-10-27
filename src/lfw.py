062d6c13c791f4333e1358ffdc62c61e47b663fe
Dave <heejung.park@honestfund.kr>
2023-10-27 15:54:21 +0900

    far target 0.001 -> 0.01 test

--- src/lfw.py
+++ src/lfw.py
@@ -39,8 +39,9 @@ def evaluate(embeddings, actual_issame, nrof_folds=10, distance_metric=0, subtra
     tpr, fpr, accuracy = facenet.calculate_roc(thresholds, embeddings1, embeddings2,
         np.asarray(actual_issame), nrof_folds=nrof_folds, distance_metric=distance_metric, subtract_mean=subtract_mean)
     thresholds = np.arange(0, 4, 0.001)
+    # TODO: 1e-2 -> 1e-3
     val, val_std, far = facenet.calculate_val(thresholds, embeddings1, embeddings2,
-        np.asarray(actual_issame), 1e-3, nrof_folds=nrof_folds, distance_metric=distance_metric, subtract_mean=subtract_mean)
+        np.asarray(actual_issame), 1e-2, nrof_folds=nrof_folds, distance_metric=distance_metric, subtract_mean=subtract_mean)
     return tpr, fpr, accuracy, val, val_std, far
 
 def get_paths(lfw_dir, pairs):
